adc1ca2d1f4836044eb795aac7f15e3c193771c5
Chinmay Gawde <gawdechinmay1234@gmail.com>
2024-12-28 22:27:20 +0530

    updated yaml

---
+++ app/src/main/java/com/sea/roomdbkotlin/room_db/AppDataBase.kt
@@ -0,0 +1,9 @@
+package com.sea.roomdbkotlin.room_db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [User::class], version = 1)
+abstract class AppDataBase : RoomDatabase() {
+    abstract fun userDao() : UserDao
+}
