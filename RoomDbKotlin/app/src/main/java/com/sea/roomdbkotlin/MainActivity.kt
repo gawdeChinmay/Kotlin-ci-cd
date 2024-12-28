f068c7743c7c33728372a7bb3d23fe9a4afe654a
Chinmay Gawde <gawdechinmay1234@gmail.com>
2024-12-28 18:12:03 +0530

    base code

--- RoomDbKotlin/app/src/main/java/com/sea/roomdbkotlin/MainActivity.kt
+++ RoomDbKotlin/app/src/main/java/com/sea/roomdbkotlin/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
